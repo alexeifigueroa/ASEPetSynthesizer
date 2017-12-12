a13cf986c34162bbb4a434e06e4ba2bdf3d7d381
Alexei Figueroa <alexeifigueroa@gmail.com>
2017-12-12 13:44:01 +0100

    corrected the merge

--- synthesizer.py
+++ synthesizer.py
@@ -5,7 +5,6 @@ Created on 8 Dec 2017
 #conda install -c conda-forge wxpython
 import numpy as np
 import wave
-<<<<<<< HEAD
 import io,struct
 import wx
 import wx.adv
@@ -13,22 +12,18 @@ import fractions
 import functools
 import os
 import pygame
-
-=======
 import winsound,io,struct
 import wx
 import wx.adv
 from collections import defaultdict
 import fractions
 import functools
->>>>>>> 97a1d9734520780bcfa5ba4ed594a00d48d6e415
 #https://scicomp.stackexchange.com/questions/21230/lcm-builtin-in-python-numpy
 def lcm(a,b): 
     return abs(a * b) / fractions.gcd(a,b) if a and b else 0
 def keyFreq(n):
     #0 =A0
     return np.power(2,(n-49)/12)*440#Hz
-<<<<<<< HEAD
 def opj(path):
     """Convert paths to the platform-specific separator"""
     st = os.path.join(*tuple(path.split('/')))
@@ -36,9 +31,6 @@ def opj(path):
     if path.startswith('/'):
         st = '/' + st
     return st
-
-=======
->>>>>>> 97a1d9734520780bcfa5ba4ed594a00d48d6e415
 def createSoundFreq(Fs=44100.0,key=49):
     f=keyFreq(key)    #Hz
     T=1/f #1 period
@@ -79,15 +71,12 @@ class wxHelloApp(wx.App):
               76:63,    # L -> B
               246:64    # oe -> C
               }
-<<<<<<< HEAD
         
         self.__pressedKey={}
         self.__activesignal=None
         self.__frame = wx.Frame(None, title="ASEPetSynthesizer")
-=======
         self.__pressedKey={}
         self.__frame = wx.Frame(None, title="New Stuff")
->>>>>>> 97a1d9734520780bcfa5ba4ed594a00d48d6e415
         self.__frame.Show()
         frameSizer=wx.BoxSizer(wx.VERTICAL)
         yeahButton=wx.Button(self.__frame,label="My button")
@@ -99,7 +88,6 @@ class wxHelloApp(wx.App):
         self.Bind(wx.EVT_KEY_UP, self.onKeyRelease)
         return True
     def onButtonClicked(self,event):
-<<<<<<< HEAD
         Fs=44100
         key=49
         f=keyFreq(key)    #Hz
@@ -127,7 +115,6 @@ class wxHelloApp(wx.App):
             self.__activesignal.stop()
             #self.createSignals()
         #wx.adv.Sound.Stop()
-=======
         createSoundFreq(key=49)
     def onKeyPress(self,event):
         code=event.GetKeyCode()
@@ -143,12 +130,10 @@ class wxHelloApp(wx.App):
             #self.__pressedKey[code].Stop()
             self.__pressedKey.pop(code)
             self.createSignals()
->>>>>>> 97a1d9734520780bcfa5ba4ed594a00d48d6e415
         #dial=wx.MessageDialog(self.__frame,"Here's a dialog")
         #dial.ShowModal()
     def createSignals(self):
         #createSoundFreq(key=self.__keyMap[code])
-<<<<<<< HEAD
         Fs=44100  #Sampling frequency
         Ts=1.0/Fs     #Sampling time
         #Find the mcm of the period
@@ -168,47 +153,6 @@ class wxHelloApp(wx.App):
         sound = pygame.sndarray.make_sound(np.int16(Sglobal))
         return sound
 
-
-
-
-
-
-
 if __name__ == "__main__":
     app = wxHelloApp()
     app.MainLoop()
-    
-
-
-
-=======
-        Fs=44100.0
-        
-        #Find the mcm of the period
-        
-        T=[1/keyFreq(self.__pressedKey[k])   for k in self.__pressedKey] 
-        
-        [Tglobal]=functools.reduce(lcm,T)
-        Sglobal=np.zeros(int(Tglobal/Fs))
-        for k  in self.__pressedKey:
-            f=keyFreq(self.__pressedKey[k])    #Hz
-            T=1/f #1 period
-            t=np.arange(0,T,1/Fs)
-            s=np.int16(32767.0*1/len(T)*np.cos(2*np.pi*f*t))
-            Sglobal+=s
-        x=struct.pack('<%sh'%Sglobal.size,*Sglobal)
-        
-        memory_file = io.BytesIO()
-        with wave.open(memory_file, 'wb') as f :
-            f.setnchannels(1)
-            f.setsampwidth(2)
-            f.setframerate(int(Fs))
-            f.writeframes(x)
-        sound =wx.adv.Sound()
-        sound.CreateFromData(memory_file.getvalue())
-        sound.Play(wx.adv.SOUND_ASYNC|wx.adv.SOUND_LOOP)   
-if __name__ == "__main__":
-    app = wxHelloApp()
-    app.MainLoop()
->>>>>>> 97a1d9734520780bcfa5ba4ed594a00d48d6e415
-
